a2fbaf514972a34e216436cf1185d59830ffa0df
Karamashev Leonid <leonkaram@mail.ru>
2023-04-14 12:30:11 +0700

    create base internalization logic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,30 @@
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { useTranslation } from 'next-i18next'
+
 import Logo from '@/ui/Logo'
 
-export default function Home() {
+export interface HomeProps {}
+
+export default function Home(
+  _props: InferGetServerSidePropsType<typeof getServerSideProps>
+) {
+  const { t } = useTranslation(['header'])
+
   return (
     <main>
       <Logo />
-      <h1>Clone IVI</h1>
+      <h1>{t('header:example')}</h1>
     </main>
   )
+}
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async ({
+  locale,
+}) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale ?? 'ru', ['header'])),
+    },
+  }
 }
