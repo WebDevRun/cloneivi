535ccacaf0bc8ae09edb157bce400ba11cc30170
Alexandr Panteleev <88164077+Gabageba@users.noreply.github.com>
2023-04-15 21:34:45 +0700

    chore: selector button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,12 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useTranslation } from 'next-i18next'
 import { Layout } from '@/layout/Layout'
 import Logo from '@/ui/Logo'
+import { SelectorButton } from '@/ui/SelectorButton'
+import { useState } from 'react'
 
-export default function Home() {
+const Home = () => {
   const { t } = useTranslation(['header'])
+  const [active, setActive] = useState(false)
 
   return (
     <main>
@@ -15,10 +18,15 @@ export default function Home() {
       <Layout>
         <Logo />
         <h1>Clone IVI</h1>
+        <SelectorButton name={'adsf'} active={active} setActive={setActive} selectedItems={['sadfasdf', 'asdfasdf', 'asdfsafd']}/>
+        <SelectorButton name={'adsf'} active={active} setActive={setActive} selectedItems={[]} disabled={true}/>
+        <SelectorButton name={'adsf'} active={active} setActive={setActive} selectedItems={['Детектив', 'Артхаус', 'Мультфильм', 'Триллер', 'Военные']} disabled={false}/>
       </Layout>
     </main>
   )
 }
+
+export default Home
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
   return {
