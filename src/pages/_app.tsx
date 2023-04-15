c7911578eb7f5b0865ea891619186bc405903dfc
Alexandr Panteleev <88164077+Gabageba@users.noreply.github.com>
2023-04-16 01:13:28 +0700

    chore: delete type

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
+import { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
 
-import type { AppProps } from 'next/app'
 
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
