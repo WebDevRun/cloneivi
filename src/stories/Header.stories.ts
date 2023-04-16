860dbd009c0a3219d62ce1b41c0c26ba458efe6b
Alexandr Panteleev <88164077+Gabageba@users.noreply.github.com>
2023-04-17 00:00:24 +0700

    merge fix

--- src/stories/Header.stories.ts
+++ src/stories/Header.stories.ts
@@ -1,6 +1,6 @@
-import { Header } from './Header';
+import { Meta, StoryObj } from '@storybook/react';
 
-import type { Meta, StoryObj } from '@storybook/react';
+import { Header } from './Header';
 
 const meta: Meta<typeof Header> = {
   title: 'Example/Header',
@@ -11,10 +11,10 @@ const meta: Meta<typeof Header> = {
     // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'fullscreen',
   },
-};
+}
 
-export default meta;
-type Story = StoryObj<typeof Header>;
+export default meta
+type Story = StoryObj<typeof Header>
 
 export const LoggedIn: Story = {
   args: {
@@ -22,6 +22,6 @@ export const LoggedIn: Story = {
       name: 'Jane Doe',
     },
   },
-};
+}
 
-export const LoggedOut: Story = {};
+export const LoggedOut: Story = {}
