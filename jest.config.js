bb07e6d31084e4b832a618c4253a7f9de4165f52
Karamashev Leonid <leonkaram@mail.ru>
2023-04-16 18:35:18 +0700

    chore: create aliases

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ const createJestConfig = nextJest({
 
 /** @type {import('jest').Config} */
 const config = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testEnvironment: 'jest-environment-jsdom',
 }
 
