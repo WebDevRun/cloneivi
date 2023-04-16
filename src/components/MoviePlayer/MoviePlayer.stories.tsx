e1a1c0f0af4e6ddf199be8aa1cc6bea1f683cd2f
Karamashev Leonid <leonkaram@mail.ru>
2023-04-16 20:30:51 +0700

    create player component

---
+++ src/components/MoviePlayer/MoviePlayer.stories.tsx
@@ -0,0 +1,32 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { MoviePlayer } from './MoviePlayer'
+
+const meta: Meta<typeof MoviePlayer> = {
+  title: 'MoviePlayer',
+  tags: ['autodocs'],
+  component: MoviePlayer,
+  argTypes: {
+    posterSrc: {
+      type: 'string',
+      description: 'Ссылка на постер',
+    },
+    videoSrc: {
+      type: 'string',
+      description: 'Ссылка на видео',
+    },
+  },
+}
+
+export default meta
+
+type Story = StoryObj<typeof MoviePlayer>
+
+export const Default: Story = {
+  args: {
+    videoSrc:
+      'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+    posterSrc:
+      'https://peach.blender.org/wp-content/uploads/title_anouncement.jpg?x11217',
+  },
+}
---
+++ src/components/MoviePlayer/index.ts
@@ -0,0 +1,1 @@
+export { MoviePlayer } from './MoviePlayer'
