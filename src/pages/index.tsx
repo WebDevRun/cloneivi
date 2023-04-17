171dc976a445b96b35f18bbbccd44994ce75f189
injashkin <injashkin@gmail.com>
2023-04-18 00:12:34 +0300

    Add Header to the AppLayout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { GetStaticProps } from 'next'
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
+import { Header } from '@/components/Header'
 import { AppLayout } from '@layouts/AppLayout'
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
     <main>
       <AppLayout>
         <h1>{t('header:more')}</h1>
+        <Header></Header>
       </AppLayout>
     </main>
   )
