7d5788205bd37993de2036cc3f8b31254a3b4d61
Karamashev Leonid <leonkaram@mail.ru>
2023-04-18 09:58:17 +0700

    chore: delete MovieCard component

--- src/ui/MoviePlayer/MoviePlayer.tsx
+++ src/ui/MoviePlayer/MoviePlayer.tsx
@@ -1,3 +1,4 @@
+import cn from 'classnames'
 import Image from 'next/image'
 import {
   ChangeEventHandler,
@@ -8,9 +9,9 @@ import {
   useState,
 } from 'react'
 
-import fullscreenToClose from '../../assets/images/player/fullscreen-to-close.svg'
-import fullscreenToOpen from '../../assets/images/player/fullscreen-to-open.svg'
-import play from '../../assets/images/player/play.svg'
+import fullscreenToClose from '@assets/images/player/fullscreen-to-close.svg'
+import fullscreenToOpen from '@assets/images/player/fullscreen-to-open.svg'
+import play from '@assets/images/player/play.svg'
 
 import styles from './MoviePlayer.module.scss'
 
@@ -27,6 +28,7 @@ export const MoviePlayer: FC<MoviePlayerProps> = ({ videoSrc, posterSrc }) => {
   const [playStatus, setPlayStatus] = useState<playStatusTypes>('stop')
   const [isFullscreen, setIsFullscreen] = useState<isFullscreenTypes>(undefined)
 
+  const videoLayout = useRef<HTMLDivElement>(null)
   const videoRef = useRef<HTMLVideoElement>(null)
 
   useEffect(() => {
@@ -37,11 +39,15 @@ export const MoviePlayer: FC<MoviePlayerProps> = ({ videoSrc, posterSrc }) => {
   }, [playStatus])
 
   useEffect(() => {
-    const toogleFullscreen = async () => {
-      await videoRef.current?.requestFullscreen()
+    const setFullscreen = async () => {
+      await videoLayout.current?.requestFullscreen()
+    }
+    const exitFullscreen = async () => {
+      await document.exitFullscreen()
     }
 
-    if (isFullscreen !== undefined) toogleFullscreen()
+    if (isFullscreen === true) setFullscreen()
+    if (isFullscreen === false) exitFullscreen()
   }, [isFullscreen])
 
   const playButtonClickHandler: MouseEventHandler<HTMLButtonElement> = (
@@ -53,17 +59,16 @@ export const MoviePlayer: FC<MoviePlayerProps> = ({ videoSrc, posterSrc }) => {
   const fullscreenCheckboxChangeHandler: ChangeEventHandler<
     HTMLInputElement
   > = (event) => {
-    setIsFullscreen(true)
+    setIsFullscreen((prev) => !prev)
   }
 
   return (
-    <div className={styles.moviePlayer}>
+    <div ref={videoLayout} className={styles.moviePlayer}>
       <video
         ref={videoRef}
         className={styles.player}
         preload="none"
         poster={posterSrc}
-        // controls={true} // to delete
       >
         <source src={videoSrc} />
       </video>
@@ -74,11 +79,16 @@ export const MoviePlayer: FC<MoviePlayerProps> = ({ videoSrc, posterSrc }) => {
         </button>
       )}
 
+      {playStatus === 'stop' && !isFirstPlay && (
+        <button className={styles.playButton} onClick={playButtonClickHandler}>
+          <Image className={styles.playImage} src={play} alt="play" />
+        </button>
+      )}
+
       <label className={styles.fullscreenConatiner}>
         <input
           className={styles.fullscreenCheckbox}
           type="checkbox"
-          checked={isFullscreen}
           onChange={fullscreenCheckboxChangeHandler}
         />
         <Image
