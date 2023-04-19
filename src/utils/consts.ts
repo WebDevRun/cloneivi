d771b7e466cdb7830afce0c2bfa922e59b7e1564
Alexandr Panteleev <88164077+Gabageba@users.noreply.github.com>
2023-04-20 00:28:27 +0700

    add const

---
+++ src/utils/consts.ts
@@ -0,0 +1,71 @@
+import { FC } from 'react'
+
+export type IGenresTop = {
+  id: number
+  name: string
+  iconSvg: FC
+  iconAlt: string
+}
+
+export const GENRES_TOP: IGenresTop[] = [
+  {
+    id: 0,
+    name: 'detective',
+    iconSvg: ,
+    iconAlt: ''
+  },
+  {
+    id: 1,
+    name: 'comedy',
+    iconSvg: ,
+    iconAlt: ''
+  },
+  {
+    id: 2,
+    name: 'action',
+    iconSvg: ,
+    iconAlt: ''
+  },
+  {
+    id: 3,
+    name: 'thriller',
+    iconSvg: ,
+    iconAlt: ''
+  },
+  {
+    id: 4,
+    name: 'adventures',
+    iconSvg: ,
+    iconAlt: ''
+  },
+  {
+    id: 5,
+    name: 'melodrama',
+    iconSvg: ,
+    iconAlt: ''
+  },
+  {
+    id: 6,
+    name: 'fantastic',
+    iconSvg: ,
+    iconAlt: ''
+  },
+  {
+    id: 7,
+    name: 'fantasy',
+    iconSvg: ,
+    iconAlt: ''
+  },
+  {
+    id: 8,
+    name: 'Семейный',
+    iconSvg: ,
+    iconAlt: ''
+  },
+  {
+    id: 9,
+    name: '',
+    iconSvg: ,
+    iconAlt: ''
+  },
+]
