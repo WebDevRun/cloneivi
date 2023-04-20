de5029634cb6430450e2903add9672fb4694aa3c
Alexandr Panteleev <88164077+Gabageba@users.noreply.github.com>
2023-04-20 19:44:46 +0700

    add genres

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 import { AppLayout } from '@layouts/AppLayout'
+import { Icons } from '@ui/temp/Icons'
 
 export default function Home() {
   const { t } = useTranslation(['header'])
@@ -11,6 +12,7 @@ export default function Home() {
     <main>
       <AppLayout>
         <h1>{t('header:more')}</h1>
+        <Icons />
       </AppLayout>
     </main>
   )
--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,71 +1,136 @@
-import { FC } from 'react'
-
-export type IGenresTop = {
+export type IGenresIcons = {
   id: number
   name: string
-  iconSvg: FC
-  iconAlt: string
 }
 
-export const GENRES_TOP: IGenresTop[] = [
+export const GENRES_ICONS: IGenresIcons[] = [
   {
     id: 0,
-    name: 'detective',
-    iconSvg: ,
-    iconAlt: ''
+    name: 'anime'
   },
   {
     id: 1,
-    name: 'comedy',
-    iconSvg: ,
-    iconAlt: ''
+    name: 'biography'
   },
   {
     id: 2,
-    name: 'action',
-    iconSvg: ,
-    iconAlt: ''
+    name: 'action'
   },
   {
     id: 3,
-    name: 'thriller',
-    iconSvg: ,
-    iconAlt: ''
+    name: 'western'
   },
   {
     id: 4,
-    name: 'adventures',
-    iconSvg: ,
-    iconAlt: ''
+    name: 'military'
   },
   {
     id: 5,
-    name: 'melodrama',
-    iconSvg: ,
-    iconAlt: ''
+    name: 'detective'
   },
   {
     id: 6,
-    name: 'fantastic',
-    iconSvg: ,
-    iconAlt: ''
+    name: 'children'
   },
   {
     id: 7,
-    name: 'fantasy',
-    iconSvg: ,
-    iconAlt: ''
+    name: 'adults'
   },
   {
     id: 8,
-    name: 'Семейный',
-    iconSvg: ,
-    iconAlt: ''
+    name: 'documentary'
   },
   {
     id: 9,
-    name: '',
-    iconSvg: ,
-    iconAlt: ''
+    name: 'drama'
   },
+  {
+    id: 10,
+    name: 'game'
+  },
+  {
+    id: 11,
+    name: 'history'
+  },
+  {
+    id: 12,
+    name: 'comedy'
+  },
+  {
+    id: 13,
+    name: 'concert'
+  },
+  {
+    id: 14,
+    name: 'shortFilm'
+  },
+  {
+    id: 15,
+    name: 'crime'
+  },
+  {
+    id: 16,
+    name: 'melodrama'
+  },
+  {
+    id: 17,
+    name: 'music'
+  },
+  {
+    id: 18,
+    name: 'cartoons'
+  },
+  {
+    id: 19,
+    name: 'musical'
+  },
+  {
+    id: 20,
+    name: 'news'
+  },
+  {
+    id: 21,
+    name: 'adventures'
+  },
+  {
+    id: 22,
+    name: 'realTV'
+  },
+  {
+    id: 23,
+    name: 'family'
+  },
+  {
+    id: 24,
+    name: 'sport'
+  },
+  {
+    id: 25,
+    name: 'talkShow'
+  },
+  {
+    id: 26,
+    name: 'thriller'
+  },
+  {
+    id: 27,
+    name: 'horror'
+  },
+  {
+    id: 28,
+    name: 'fantastic'
+  },
+  {
+    id: 29,
+    name: 'noirFilm'
+  },
+  {
+    id: 30,
+    name: 'fantasy'
+  },
+  {
+    id: 31,
+    name: 'ceremony'
+  },
+
 ]
