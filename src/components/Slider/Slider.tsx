11aedbce8e2c7071634d858031bc5b3ff27e2f0c
Alexandr Panteleev <88164077+Gabageba@users.noreply.github.com>
2023-04-21 22:51:58 +0700

    chore: add slider adaptive

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -7,14 +7,17 @@ import styles from './Slider.module.scss'
 
 export interface SliderProps {
   Component: FC
-  componentSetting?: object
   items: any[]
+  arrowSize: 'small' | 'big'
+  type: 'list' | 'oneItem'
+  componentSetting?: object
   onItemClick: (id: number) => void
   slidesToShow?: number
   slidesToScroll?: number
   startPosition?: number
   gap?: number
-  arrowSize?: 'small' | 'big'
+  infinite?: boolean
+  cloneCount?: string
 }
 
 export const Slider: FC<SliderProps> = ({
@@ -25,9 +28,13 @@ export const Slider: FC<SliderProps> = ({
   slidesToShow,
   startPosition = 0,
   slidesToScroll,
-  arrowSize = 'big',
+  arrowSize,
   gap = 24,
+  infinite = false,
+  type,
+  cloneCount = 2,
 }) => {
+  const TRANSITION = 0.6
   const container = useRef<HTMLDivElement | null>(null)
   const track = useRef<HTMLDivElement | null>(null)
   const [itemWidth, setItemWidth] = useState<number>(0)
@@ -35,30 +42,62 @@ export const Slider: FC<SliderProps> = ({
   const [itemsGap, setItemsGap] = useState<number>(gap || 24)
   const [position, setPosition] = useState<number>(startPosition || 0)
   const [scrollStep, setScrollStep] = useState<number>(slidesToScroll || 0)
+  const [transitionDuration, setTransitionDuration] = useState<number>(0)
+  const [sliderItems, setSliderItems] = useState<typeof items[]>([])
+  // const [cloneCount, setCloneCount] = useState<number>(cloneCount)
 
   useEffect(() => {
-    const item = track.current?.firstElementChild?.firstElementChild
-    const itemClientWidth = (item as HTMLElement)?.offsetWidth
-
-    if (slidesCount) {
-      setSettings(slidesCount, itemClientWidth)
-    } else {
-      const slideCount = Math.floor((container.current?.clientWidth + gap) / (itemClientWidth + gap))
-      if (itemClientWidth) {
-        setSettings(slideCount, itemClientWidth)
-      }
+    window.addEventListener('resize', sliderSetting)
+    sliderSetting()
+    return () => {
+      window.removeEventListener('resize', sliderSetting)
     }
   }, [])
 
+  useEffect(() => {
+    if (infinite) {
+
+    }
+
+    setSliderItems(items)
+  }, [items, infinite])
+
+  const sliderSetting = () => {
+    setTransitionDuration(0)
+    const item = track.current?.firstElementChild?.firstElementChild
+    const itemClientWidth = (item as HTMLElement)?.offsetWidth
+
+    if (type === 'list') {
+      if (slidesCount) {
+        setSettings(slidesCount, itemClientWidth)
+      } else {
+        const slideCount = Math.floor((container.current?.clientWidth + gap) / (itemClientWidth + gap))
+
+        if (itemClientWidth) {
+          setSettings(slideCount, itemClientWidth)
+        }
+      }
+    }
+
+    if (type === 'oneItem') {
+
+    }
+
+    setTimeout(() => {
+      setTransitionDuration(TRANSITION)
+    }, TRANSITION)
+  }
+
   const setSettings = (showSlides, itemWidth) => {
     const clientWidth = container.current?.clientWidth
-    const gap = (clientWidth - showSlides * itemWidth) / (showSlides - 1)
+    const gap = showSlides > 1 ? (clientWidth - showSlides * itemWidth) / (showSlides - 1) : clientWidth - showSlides * itemWidth
     const startPost = getStartPosition(showSlides)
+
     setSlidesCount(showSlides)
     setItemWidth(itemWidth)
     setPosition(-startPost * (itemWidth + gap) || 0)
-
     setItemsGap(gap)
+
     if (!scrollStep || scrollStep > showSlides) {
       setScrollStep(showSlides > 1 ? showSlides - 1 : 1)
     }
@@ -66,24 +105,29 @@ export const Slider: FC<SliderProps> = ({
 
   const getStartPosition = (showSlides) => {
     let startPos = startPosition
+
     if (startPosition < 0) {
       startPos = 0
     }
+
     if (startPosition > items.length - showSlides) {
       startPos = items.length - showSlides
     }
+
     return startPos
   }
 
   const nextClickHandler = () => {
     const itemsLeft = items.length - ((Math.abs(position) + slidesCount * (itemWidth + itemsGap)) / (itemWidth + itemsGap))
     const pos = itemsLeft >= scrollStep ? scrollStep * (itemWidth + itemsGap) : itemsLeft * (itemWidth + itemsGap)
+
     setPosition(prevState => prevState - pos)
   }
 
   const prevClickHandler = () => {
     const itemsLeft = Math.abs(position) / (itemWidth + itemsGap)
     const pos = itemsLeft >= scrollStep ? scrollStep * (itemWidth + itemsGap) : itemsLeft * (itemWidth + itemsGap)
+
     setPosition(prevState => prevState + pos)
   }
 
@@ -93,13 +137,18 @@ export const Slider: FC<SliderProps> = ({
         position !== 0 &&
         <button className={cn(styles.button, styles[`${arrowSize}ButtonLeft`])} onClick={prevClickHandler}>
           <ArrowSvg color='#BCBCBF'
-                    size={arrowSize || 'big'}
+                    size={arrowSize}
                     direction='left'
           />
         </button>
       }
       <div className={styles.container} ref={container}>
-        <div className={styles.track} style={{gap: itemsGap, transform: `translateX(${position}px)`}}
+        <div className={styles.track}
+             style={{
+               gap: itemsGap,
+               transform: `translateX(${position}px)`,
+               transition: `${transitionDuration}s`,
+             }}
              ref={track}>
           {
             items.map(item => (
@@ -115,7 +164,7 @@ export const Slider: FC<SliderProps> = ({
         <button className={cn(styles.button, styles[`${arrowSize}ButtonRight`])}
                 onClick={nextClickHandler}>
           <ArrowSvg color={'#BCBCBF'}
-                    size={arrowSize || 'big'}
+                    size={arrowSize}
                     direction='right'
           />
         </button>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,19 @@ import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 import { AppLayout } from '@layouts/AppLayout'
+import { Slider } from '@components/Slider'
+import { SliderButton } from '@ui/SliderButton/SliderButton'
+import { GENRES_ICONS } from '@/utils/consts/genres'
 
 const Home = () => {
-  const { t } = useTranslation( ['header'] )
+  const {t} = useTranslation(['header'])
 
   return (
     <main>
       <AppLayout>
-        <h1>{t( 'header:more' )}</h1>
+        <h1>{t('header:more')}</h1>
+        <Slider Component={SliderButton} items={GENRES_ICONS} arrowSize={'big'} type={'list'} onItemClick={() => ''}
+                componentSetting={{type: 'square', style: 'fill'}} />
       </AppLayout>
     </main>
   )
@@ -18,8 +23,8 @@ const Home = () => {
 
 export default Home
 
-export const getStaticProps: GetStaticProps = async ({ locale }) => {
-  const localeData = await serverSideTranslations( locale ?? 'ru', ['header'] )
+export const getStaticProps: GetStaticProps = async ({locale}) => {
+  const localeData = await serverSideTranslations(locale ?? 'ru', ['header'])
   return {
     props: {
       ...localeData,
