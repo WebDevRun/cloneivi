6b0bd135e99eb01531cfe788f1fb26c16c8388c5
Alexandr Panteleev <88164077+Gabageba@users.noreply.github.com>
2023-04-26 18:10:39 +0700

    refactor: refactor checkbox styles

--- src/ui/Select/Select.tsx
+++ src/ui/Select/Select.tsx
@@ -11,11 +11,14 @@ export interface SelectProps {
 
 export const Select: FC<SelectProps> = ({type, text, disabled = false}) => {
   return (
-    <label className={cn(
-      styles.select,
-      styles[type],
-      disabled && styles.disabled,
-    )} style={{cursor: disabled ? 'default' : 'pointer'}}>
+    <label
+      className={cn(
+        styles.select,
+        styles[type],
+        disabled && styles.disabled,
+      )}
+      style={{cursor: disabled ? 'default' : 'pointer'}}
+    >
       <input type={type} className={styles.input} disabled={disabled} />
       <span className={styles.text}>{text}</span>
     </label>
