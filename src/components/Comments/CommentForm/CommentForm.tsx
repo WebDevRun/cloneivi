1207dbfbe4d1e15e0834c9a39b98dcf82d8eebd8
Karamashev Leonid <leonkaram@mail.ru>
2023-04-27 21:16:10 +0700

    feat: append ui logic to CommentForm

--- src/components/Comments/CommentForm/CommentForm.tsx
+++ src/components/Comments/CommentForm/CommentForm.tsx
@@ -1,22 +1,80 @@
-import { FC } from 'react'
+import cn from 'classnames'
+import { ChangeEventHandler, FC, useEffect, useState } from 'react'
 
 import { CommentAvatar } from '../CommentAvatar'
 
 import styles from './CommentForm.module.scss'
 
+const textLengthLimit = 10
+
 export const CommentForm: FC = () => {
+  const [text, setText] = useState('')
+  const [isError, setIsError] = useState(false)
+  const [isDisabled, setIsDisabled] = useState(true)
+
+  useEffect(() => {
+    if (text.length >= textLengthLimit) {
+      setIsDisabled(false)
+      setIsError(false)
+      return
+    }
+
+    if (text.length === 0) {
+      setIsError(false)
+      setIsDisabled(true)
+      return
+    }
+
+    setIsError(true)
+    setIsDisabled(true)
+  }, [text])
+
+  const inputChangeHandler: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setText(event.currentTarget.value)
+  }
+
   return (
     <form className={styles.commentForm}>
       <CommentAvatar />
 
-      <label className={styles.label}>
-        <input className={styles.input} type='text' />
-        <p className={styles.placeholder}>Написать отзыв</p>
-      </label>
+      <div className={styles.inputContainer}>
+        <label className={styles.label}>
+          <input
+            className={cn(styles.input, {
+              [styles.error]: isError,
+            })}
+            type='text'
+            value={text}
+            onChange={inputChangeHandler}
+          />
+          <p
+            className={cn(styles.placeholder, {
+              [styles.hasText]: text,
+              [styles.error]: isError,
+            })}
+          >
+            Написать отзыв
+          </p>
+        </label>
 
-      <button className={styles.button} type='submit'>
-        Отправить
-      </button>
+        {text.length > 0 && text.length < textLengthLimit && (
+          <p className={styles.caption}>
+            Минимум {textLengthLimit} символов, вы ввели {text.length}
+          </p>
+        )}
+      </div>
+
+      <div>
+        <button
+          className={cn(styles.button, {
+            [styles.disabled]: isDisabled,
+          })}
+          type='submit'
+          disabled={isDisabled}
+        >
+          Отправить
+        </button>
+      </div>
     </form>
   )
 }
