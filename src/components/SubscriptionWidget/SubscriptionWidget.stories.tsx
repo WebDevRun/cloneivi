c873e4996b080bf21055336d8be177fa1c667088
injashkin <injashkin@gmail.com>
2023-04-27 14:49:16 +0300

    Add SubscriptionWidget

---
+++ src/components/SubscriptionWidget/SubscriptionWidget.stories.tsx
@@ -0,0 +1,31 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { SubscriptionWidget } from './SubscriptionWidget'
+import '@styles/index.scss'
+
+const meta: Meta<typeof SubscriptionWidget> = {
+  title: 'SubscriptionWidget',
+  component: SubscriptionWidget,
+  tags: ['autodocs'],
+  argTypes: {},
+  parameters: {
+    backgrounds: {
+      default: 'twitter',
+      values: [
+        { name: 'twitter', value: '#00aced' },
+        { name: 'facebook', value: '#3b5998' },
+      ],
+    },
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof SubscriptionWidget>
+
+export const Widget: Story = {
+  args: {
+    title: 'Подписка Иви',
+    text: 'От 199 ₽ за месяц',
+    textButton: 'Смотреть на SmartTV',
+  },
+}
