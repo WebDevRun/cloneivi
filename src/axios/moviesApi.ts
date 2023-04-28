e964faa910a54eb3ceb8e546181c43d899a221cd
Karamashev Leonid <leonkaram@mail.ru>
2023-04-28 19:15:03 +0700

    fix: update file name

--- src/axios/moviesApi.ts
+++ src/axios/moviesApi.ts
@@ -1,5 +1,5 @@
 import { $instance } from '@/axios/index'
-import { IMovie } from '@/types/Movie'
+import { IMovie } from '@/types/movie'
 
 export const getFilms = async (): Promise<IMovie[]> => {
   const { data } = await $instance.get('movies/movies.json')
