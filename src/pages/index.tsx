28b72976f07a18f0cedcb4b36c7d20ed36287904
injashkin <injashkin@gmail.com>
2023-05-14 23:40:54 +0300

    Make a big widget

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,11 @@ import { GetStaticProps } from 'next'
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
+import { SubscriptionWidget } from '@/components/SubscriptionWidget'
 import { Header } from '@components/Header'
 import { AppLayout } from '@layouts/AppLayout'
+
+import posters from './../components/SubscriptionWidget/posters.json'
 
 export default function Home() {
   const { t } = useTranslation(['header'])
@@ -13,6 +16,21 @@ export default function Home() {
       <AppLayout>
         <h1>{t('header:more')}</h1>
         <Header />
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <SubscriptionWidget
+            posters={posters.subscription}
+            size='big'
+            textButton='Другие подписки'
+          />
+
+          <SubscriptionWidget
+            posters={posters.movies}
+            note='Отключить можно в любой момент'
+            text='От 199 ₽ за месяц'
+            textButton='Смотреть на SmartTV'
+            title='Подписка Иви'
+          />
+        </div>
       </AppLayout>
     </main>
   )
