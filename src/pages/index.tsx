8f96382e7697b4d5700644e7c7c9b0e416de59fb
Ilham Zeynalov <ilhamzeynalov65@gmail.com>
2023-05-15 18:07:40 +0400

    refactor: index.ts file cancel changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import { Header } from '@components/Header'
 import { AppLayout } from '@layouts/AppLayout'
 import { GetStaticProps } from 'next'
+import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 export default function Home() {
+  const { t } = useTranslation(['header'])
+
   return (
     <main>
       <AppLayout>
+        <h1>{t('header:more')}</h1>
         <Header />
       </AppLayout>
     </main>
