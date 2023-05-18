54bdb934757b8a1fedf690036143b976f1b76fe6
injashkin <injashkin@gmail.com>
2023-05-18 14:55:28 +0300

    Refactor code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,8 @@ import { GetStaticProps } from 'next'
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
-import { TextCollapse } from '@/components/TextCollapse'
 import { Header } from '@components/Header'
 import { AppLayout } from '@layouts/AppLayout'
-
-import data from '../components/TextCollapse/data.json'
 
 export default function Home() {
   const { t } = useTranslation(['header'])
