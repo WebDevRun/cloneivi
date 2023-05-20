0632d439b0d299c0eee37d53acd593dc546d06fd
injashkin <injashkin@gmail.com>
2023-05-21 02:21:27 +0300

    AppLayout is made by the main wrapper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,16 @@
 import { GetStaticProps } from 'next'
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import { Fragment } from 'react'
 
-import { Header } from '@/components/Header'
-import { HeaderDropdown } from '@/components/HeaderDropdown/HeaderDropdown'
-import { DropDownLayout } from '@/layouts/DropDownLayout'
 import { AppLayout } from '@layouts/AppLayout'
 
 export default function Home() {
   const { t } = useTranslation(['header'])
 
   return (
-    <Fragment>
-      <header>
-        <AppLayout>
-          <Header />
-          <DropDownLayout position='center' size='small' type='header'>
-            <HeaderDropdown />
-          </DropDownLayout>
-        </AppLayout>
-      </header>
-      <main>
-        <AppLayout>
-          <h1>{t('header:more')}</h1>
-        </AppLayout>
-      </main>
-    </Fragment>
+    <AppLayout>
+      <h1>{t('header:more')}</h1>
+    </AppLayout>
   )
 }
 
