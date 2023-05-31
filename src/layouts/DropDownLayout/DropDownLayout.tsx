96e565b7523aaa47fb10195f2b2ec75624ffb0a8
Alexandr Panteleev <gabageba@gmail.com>
2023-05-31 21:08:36 +0700

    main fix merge conflict

--- src/layouts/DropDownLayout/DropDownLayout.tsx
+++ src/layouts/DropDownLayout/DropDownLayout.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import cn from 'classnames'
 import { FC, ReactNode } from 'react'
 
@@ -7,36 +6,6 @@ import styles from './DropDownLayout.module.scss'
 export interface ModalLayoutProps {
   children: ReactNode
   size?: 'big' | 'small' | 'middle'
-  position?: 'left' | 'center' | 'right'
-  type: 'filter' | 'header'
-}
-
-export const DropDownLayout: FC<ModalLayoutProps> = ({
-  children,
-  position = 'center',
-  size = 'big',
-  type,
-}) => {
-  return (
-    <div className={cn(
-      styles.dropDownLayout,
-      styles[type],
-      styles[size],
-      styles[position],
-    )}>
-      {children}
-    </div>
-  )
-}
-=======
-import cn from 'classnames'
-import { FC, ReactNode } from 'react'
-
-import styles from './DropDownLayout.module.scss'
-
-export interface ModalLayoutProps {
-  children: ReactNode
-  size?: 'big' | 'small'
   position?: 'left' | 'center' | 'right'
   type: 'filter' | 'header'
 }
@@ -53,11 +22,10 @@ export const DropDownLayout: FC<ModalLayoutProps> = ({
         styles.dropDownLayout,
         styles[type],
         size && styles[size],
-        position && styles[position]
+        position && styles[position],
       )}
     >
       {children}
     </div>
   )
 }
->>>>>>> 70112db8366b4d0ff8109547858b93c6e6e07ded
