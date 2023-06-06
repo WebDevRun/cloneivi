8ddb79208e7e261faa4054ab40802a76878c5134
Karamashev Leonid <leonkaram@mail.ru>
2023-06-06 15:41:22 +0700

    refactor: store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,21 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import { createWrapper } from 'next-redux-wrapper'
 
 import { filmsApi } from './endpoints/films'
 import { genresApi } from './endpoints/genres'
 
-export const makeStore = () =>
-  configureStore({
-    reducer: {
-      [filmsApi.reducerPath]: filmsApi.reducer,
-      [genresApi.reducerPath]: genresApi.reducer,
-    },
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat(filmsApi.middleware, genresApi.middleware),
-  })
+const rootReducer = combineReducers({
+  [filmsApi.reducerPath]: filmsApi.reducer,
+  [genresApi.reducerPath]: genresApi.reducer,
+})
 
-export type AppStore = ReturnType<typeof makeStore>
-export type RootState = ReturnType<AppStore['getState']>
+const middleware = [filmsApi.middleware, genresApi.middleware]
+
+export const setupStore = () => {
+  return configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(...middleware),
+  })
+}
+
+export type AppStore = ReturnType<typeof setupStore>
+export type RootState = ReturnType<typeof rootReducer>
 export type AppDispatch = AppStore['dispatch']
 
-export const wrapper = createWrapper<AppStore>(makeStore)
+export const wrapper = createWrapper<AppStore>(setupStore)
