7b376f34213ae9dce71ea5aab3d440ae27b5d0e8
injashkin <injashkin@gmail.com>
2023-07-01 10:44:45 +0300

    Fixed a long loading bug

--- src/components/Person/PersonFilmographyItem/PersonFilmographyItem.tsx
+++ src/components/Person/PersonFilmographyItem/PersonFilmographyItem.tsx
@@ -2,15 +2,15 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useTranslation } from 'next-i18next'
-import { FC, useEffect, useState } from 'react'
+import { FC } from 'react'
 
-import { IFilm } from '@/types/person'
+import { useGetFilmByIdQuery } from '@/store/endpoints/films'
+import { IMovie } from '@/types/movie'
 import { Button } from '@/ui/Button'
 import { isSvg } from '@/utils/functions/isSvg'
 
 import noImage from '../../../assets/images/no_image.jpg'
 
-import initData from './data_mock.json'
 import styles from './PersonFilmographyItem.module.scss'
 
 export interface PersonFilmographyItemProps {
@@ -25,43 +25,33 @@ export const PersonFilmographyItem: FC<PersonFilmographyItemProps> = ({
   const { t } = useTranslation()
   const { locale } = useRouter()
 
-  const [films, setFilms] = useState<IFilm>(initData)
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(`${pathDataSrc}${id}`)
-
-      const data = await response.json()
-      setFilms(data)
-    }
-
-    fetchData()
-  }, [id, pathDataSrc])
+  const filmData = useGetFilmByIdQuery(id)
+  const film = filmData.data as IMovie
 
   return (
     <div className={styles.personFilmographyItem}>
       <Link href={`${pathDataSrc}${id}`} className={styles.body}>
         <div className={styles.figure}>
-          {!isSvg(films.img) && (
+          {!isSvg(film?.img) && (
             <Image
               className={styles.image}
               src={
-                films.img
-                  ? films.img.indexOf('https://') === -1
-                    ? `https:${films.img}`
-                    : films.img
+                film?.img
+                  ? film.img.indexOf('https://') === -1
+                    ? `https:${film.img}`
+                    : film.img
                   : noImage
               }
-              alt={`${t('posterForTheMovie')} ${films.name_ru}`}
+              alt={`${t('posterForTheMovie')} ${film?.name_ru}`}
               width={75}
               height={118}
             />
           )}
-          {isSvg(films.img) && (
+          {isSvg(film?.img) && (
             <Image
               className={styles.image}
-              src={films.img ? `${films.img}` : noImage}
-              alt={`${t('posterForTheMovie')} ${films.name_ru}`}
+              src={film.img ? `${film.img}` : noImage}
+              alt={`${t('posterForTheMovie')} ${film.name_ru}`}
               width={75}
               height={118}
             />
@@ -69,15 +59,15 @@ export const PersonFilmographyItem: FC<PersonFilmographyItemProps> = ({
         </div>
         <div className={styles.main}>
           <div className={styles.info}>
-            <div className={styles.year}>{films.year}</div>
-            <div className={styles.title} title={films.name_ru}>
-              {locale === 'ru' ? films.name_ru : films.name_en}
+            <div className={styles.year}>{film?.year}</div>
+            <div className={styles.title} title={film?.name_ru}>
+              {locale === 'ru' ? film?.name_ru : film?.name_en}
             </div>
             <div className={styles.rating}>
               <span className={styles.ratingLabel}>{t('IviRating')}:</span>
               <span className={styles.ratingValue}>
                 <span className={styles.ratingValueInteger}>
-                  {films.rating}
+                  {film?.rating}
                 </span>
               </span>
             </div>
