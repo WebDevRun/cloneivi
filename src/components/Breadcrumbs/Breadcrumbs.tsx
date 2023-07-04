5deaa76cd05840d8b22fa8c4978a873efae541a1
injashkin <injashkin@gmail.com>
2023-07-04 11:14:46 +0300

    Update Breadcrumbs

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -12,23 +12,32 @@ export type CrumbItem = {
 export interface BreadcrumbsProps {
   items: CrumbItem[]
   separator: 'slash' | 'dot'
+  lastAsLink?: boolean
 }
 
 export const Breadcrumbs: FC<BreadcrumbsProps> = ({
   items,
   separator,
+  lastAsLink = false,
 }: BreadcrumbsProps) => {
   return (
     <ul className={styles.breadcrumbs}>
       {items.map((crumb, i) => (
         <li key={i} className={cn(styles.item, styles[separator])}>
           {/** Обернуть в компонент LinkBtn */}
-          {i !== items.length - 1 && (
+
+          {lastAsLink && (
             <Link href={crumb.path} className={styles.link}>
               {crumb.text}
             </Link>
           )}
-          {i === items.length - 1 && crumb.text}
+
+          {!lastAsLink && i !== items.length - 1 && (
+            <Link href={crumb.path} className={styles.link}>
+              {crumb.text}
+            </Link>
+          )}
+          {!lastAsLink && i === items.length - 1 && crumb.text}
         </li>
       ))}
     </ul>
